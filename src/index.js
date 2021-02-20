306979c3516fd53b22d6808fad11a9aff81000f2
Tulio Duarte <maqtulio@gmail.com>
2021-02-20 18:35:28 -0300

    Validators to new package

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import { IParams } from "./IParams.js";
 import { registeredParamValidators } from './decorators.js';
-export function validate(paramClass, params, options) {
-    if (!(params instanceof IParams) || !Object.keys(registeredParamValidators).includes(params.constructor.name)) {
-        throw new Error(`Please decorate your params and extend IParams => ${params.constructor.name}.`);
+export function validate(params, options) {
+    const paramClassName = params.constructor.name;
+    if (!(params instanceof IParams) || !Object.keys(registeredParamValidators).includes(paramClassName)) {
+        throw new Error(`Please decorate your params and extend IParams => ${paramClassName}.`);
     }
-    return registeredParamValidators[paramClass.name].validate(params, options);
+    return registeredParamValidators[paramClassName].validate(params, options);
 }
 export { IParams } from "./IParams.js";
 export { registeredParamValidators } from './decorators.js';
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
 import { IParams } from "./IParams.js"
 import { registeredParamValidators } from './decorators.js';
+import Joi from "joi";
 
 
-export function validate(paramClass: any, params: any, options: any) {
+export function validate(params: any, options: Joi.ValidationOptions | undefined) {
 
-    if (!(params instanceof IParams) || !Object.keys(registeredParamValidators).includes(params.constructor.name)) {
-        throw new Error(`Please decorate your params and extend IParams => ${params.constructor.name}.`)
+    const paramClassName = params.constructor.name;
+    if (!(params instanceof IParams) || !Object.keys(registeredParamValidators).includes(paramClassName)) {
+        throw new Error(`Please decorate your params and extend IParams => ${paramClassName}.`)
     }
 
-    return registeredParamValidators[paramClass.name].validate(params,options);
+    return registeredParamValidators[paramClassName].validate(params, options);
 }
 
 export { IParams } from "./IParams.js"
