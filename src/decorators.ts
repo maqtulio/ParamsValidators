df5f310088b413bd0769c238fa9e1cf2fdf3c35c
Tulio Duarte <maqtulio@gmail.com>
2021-02-20 18:18:33 -0300

    Validators to new package

--- src/decorators.ts
+++ src/decorators.ts
@@ -1,16 +1,17 @@
+import Joi from "joi";
 import "reflect-metadata";
 import { IParams } from "./IParams.js";
 
 export const registeredParamValidators: { [key: string]: any } = {}
 
 /**
- * @name paramsClass ParamsClass used on the route to be decorated. 
+ * @name validationSchema ParamsClass used on the route to be decorated. 
  */
-export const ParmasValidator = (paramsClass: any) => {
+export const ParamsValidator = (validationSchema: Joi.Schema) => {
     return function (target: any) {
         if (Object.getPrototypeOf(target.prototype).constructor.name !== IParams.name) {
             throw new Error(`Decorated paramClass ${target.name} must extend IParams`)
         };
-        registeredParamValidators[paramsClass.name] = target;
+        registeredParamValidators[target.name] = validationSchema;
     };
 };
