7681e18e1d6eb7de0d5546b0ebb2194595a0cc08
Tulio Duarte <maqtulio@gmail.com>
2021-02-20 18:32:21 -0300

    Validators to new package

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
-export { registeredParamValidators } from './decorators.js';
+import { IParams } from "./IParams.js";
+import { registeredParamValidators } from './decorators.js';
+export function validate(paramClass, params, options) {
+    if (!(params instanceof IParams) || !Object.keys(registeredParamValidators).includes(params.constructor.name)) {
+        throw new Error(`Please decorate your params and extend IParams => ${params.constructor.name}.`);
+    }
+    return registeredParamValidators[paramClass.name].validate(params, options);
+}
 export { IParams } from "./IParams.js";
+export { registeredParamValidators } from './decorators.js';
