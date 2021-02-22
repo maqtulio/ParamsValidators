0350ac8a4beebf889b07686a6bc1877ef4985ae3
Tulio Duarte <maqtulio@gmail.com>
2021-02-22 01:14:19 -0300

    Validators to new package

--- src/index.js
+++ src/index.js
@@ -7,5 +7,26 @@ export function validate(params, options) {
     }
     return registeredParamValidators[paramClassName].validate(params, options);
 }
+export function registerControllers(commonPath, server, controllers) {
+    controllers.forEach(controller => {
+        const instance = new controller();
+        // The prefix saved to our controller
+        const prefix = Reflect.getMetadata('prefix', controller);
+        // Our `routes` array containing all our routes for this controller
+        const routes = Reflect.getMetadata('routes', controller);
+        // Iterate over all routes and register them to our express application 
+        Object.keys(routes).forEach(methodName => {
+            // It would be a good idea at this point to substitute the `app[route.requestMethod]` with a `switch/case` statement
+            // since we can't be sure about the availability of methods on our `app` object. But for the sake of simplicity
+            // this should be enough for now.
+            const httpMethod = routes[methodName].requestMethod;
+            const path = routes[methodName].path;
+            server[httpMethod](commonPath + prefix + path, (req, res) => {
+                // Execute our method for this path and pass our express request and response object.
+                instance[methodName](req, res);
+            });
+        });
+    });
+}
 export { IParams } from "./IParams.js";
 export { ParamsValidator, Controller, Method, Path } from './decorators.js';
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
+import { Application, Request, Response } from "express";
 import { IParams } from "./IParams.js"
 import { registeredParamValidators } from './decorators.js';
 import Joi from "joi";
+import { RouteDefinition } from "./RouteDefinition.js";
 
 
 export function validate(params: any, options: Joi.ValidationOptions | undefined): Joi.ValidationResult {
@@ -11,6 +13,30 @@ export function validate(params: any, options: Joi.ValidationOptions | undefined
     }
 
     return registeredParamValidators[paramClassName].validate(params, options);
+}
+
+export function registerControllers(commonPath: string, server: Application, controllers: any[]): void {
+    controllers.forEach(controller => {
+        const instance: any = new controller();
+        // The prefix saved to our controller
+        const prefix = Reflect.getMetadata('prefix', controller);
+        // Our `routes` array containing all our routes for this controller
+        const routes: { [key: string]: RouteDefinition } = Reflect.getMetadata('routes', controller);
+
+        // Iterate over all routes and register them to our express application 
+        Object.keys(routes).forEach(methodName => {
+            // It would be a good idea at this point to substitute the `app[route.requestMethod]` with a `switch/case` statement
+            // since we can't be sure about the availability of methods on our `app` object. But for the sake of simplicity
+            // this should be enough for now.
+            const httpMethod = routes[methodName].requestMethod;
+            const path = routes[methodName].path;
+
+            server[httpMethod](commonPath + prefix + path, (req: Request, res: Response) => {
+                // Execute our method for this path and pass our express request and response object.
+                instance[methodName](req, res);
+            });
+        });
+    })
 }
 
 export { IParams } from "./IParams.js"
