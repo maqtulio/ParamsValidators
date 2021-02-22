9d263f3a4d11e44662c634465cc1f3413935fcd6
Tulio Duarte <maqtulio@gmail.com>
2021-02-22 00:39:52 -0300

    Validators to new package

--- src/decorators.js
+++ src/decorators.js
@@ -19,26 +19,45 @@ export const Controller = (prefix = '') => {
         // Since routes are set by our methods this should 
         //almost never be true (except the controller has no methods)
         if (!Reflect.hasMetadata('routes', target)) {
-            Reflect.defineMetadata('routes', [], target);
+            Reflect.defineMetadata('routes', {}, target);
         }
     };
 };
-export const Method = (path, requestMethod) => {
+export const Method = (requestMethod) => {
     // `target` equals our class, `propertyKey` equals our decorated method name
     return (target, propertyKey) => {
         // In case this is the first route to be
         // registered the `routes` metadata is likely to be undefined at this point.
         // To prevent any further validation simply set it to an empty array here.
         if (!Reflect.hasMetadata('routes', target.constructor)) {
-            Reflect.defineMetadata('routes', [], target.constructor);
+            Reflect.defineMetadata('routes', {}, target.constructor);
         }
         // Get the routes stored so far, extend it by the new route and re-set the metadata.
         const routes = Reflect.getMetadata('routes', target.constructor);
-        routes.push({
+        routes[propertyKey] = {
+            ...routes[propertyKey],
             requestMethod,
+            methodName: propertyKey
+        };
+        Reflect.defineMetadata('routes', routes, target.constructor);
+    };
+};
+export const Path = (path) => {
+    // `target` equals our class, `propertyKey` equals our decorated method name
+    return (target, propertyKey) => {
+        // In case this is the first route to be
+        // registered the `routes` metadata is likely to be undefined at this point.
+        // To prevent any further validation simply set it to an empty array here.
+        if (!Reflect.hasMetadata('routes', target.constructor)) {
+            Reflect.defineMetadata('routes', {}, target.constructor);
+        }
+        // Get the routes stored so far, extend it by the new route and re-set the metadata.
+        const routes = Reflect.getMetadata('routes', target.constructor);
+        routes[propertyKey] = {
+            ...routes[propertyKey],
             path,
             methodName: propertyKey
-        });
+        };
         Reflect.defineMetadata('routes', routes, target.constructor);
     };
 };
--- src/decorators.ts
+++ src/decorators.ts
@@ -8,7 +8,7 @@ export const registeredParamValidators: { [key: string]: Joi.Schema } = {}
 /**
  * @name validationSchema ParamsClass used on the route to be decorated. 
  */
-export const ParamsValidator = (validationSchema: Joi.Schema):ClassDecorator => {
+export const ParamsValidator = (validationSchema: Joi.Schema): ClassDecorator => {
     return function (target: any) {
         if (Object.getPrototypeOf(target.prototype).constructor.name !== IParams.name) {
             throw new Error(`Decorated paramClass ${target.name} must extend IParams`)
@@ -20,33 +20,56 @@ export const ParamsValidator = (validationSchema: Joi.Schema):ClassDecorator => 
 export const Controller = (prefix: string = ''): ClassDecorator => {
     return (target: any) => {
         Reflect.defineMetadata('prefix', prefix, target);
-
         // Since routes are set by our methods this should 
         //almost never be true (except the controller has no methods)
         if (!Reflect.hasMetadata('routes', target)) {
-            Reflect.defineMetadata('routes', [], target);
+            Reflect.defineMetadata('routes', {}, target);
         }
     };
 };
 
-export const Method = (path: string, requestMethod: 'get' | 'post' | 'delete' | 'options' | 'put' | "patch"): MethodDecorator => {
+export const Method = (requestMethod: 'get' | 'post' | 'delete' | 'options' | 'put' | "patch"): MethodDecorator => {
     // `target` equals our class, `propertyKey` equals our decorated method name
     return (target, propertyKey: any): void => {
         // In case this is the first route to be
         // registered the `routes` metadata is likely to be undefined at this point.
         // To prevent any further validation simply set it to an empty array here.
         if (!Reflect.hasMetadata('routes', target.constructor)) {
-            Reflect.defineMetadata('routes', [], target.constructor);
+            Reflect.defineMetadata('routes', {}, target.constructor);
         }
 
         // Get the routes stored so far, extend it by the new route and re-set the metadata.
-        const routes = Reflect.getMetadata('routes', target.constructor) as Array<RouteDefinition>;
+        const routes = Reflect.getMetadata('routes', target.constructor) as { [key: string]: RouteDefinition };
 
-        routes.push({
+        routes[propertyKey] = {
+            ...routes[propertyKey],
             requestMethod,
+            methodName: propertyKey
+        };
+
+        Reflect.defineMetadata('routes', routes, target.constructor);
+    };
+};
+
+export const Path = (path: string): MethodDecorator => {
+    // `target` equals our class, `propertyKey` equals our decorated method name
+    return (target, propertyKey: any): void => {
+        // In case this is the first route to be
+        // registered the `routes` metadata is likely to be undefined at this point.
+        // To prevent any further validation simply set it to an empty array here.
+        if (!Reflect.hasMetadata('routes', target.constructor)) {
+            Reflect.defineMetadata('routes', {}, target.constructor);
+        }
+
+        // Get the routes stored so far, extend it by the new route and re-set the metadata.
+        const routes = Reflect.getMetadata('routes', target.constructor) as { [key: string]: RouteDefinition };
+
+        routes[propertyKey] = {
+            ...routes[propertyKey],
             path,
             methodName: propertyKey
-        });
+        };
+
         Reflect.defineMetadata('routes', routes, target.constructor);
     };
 };
